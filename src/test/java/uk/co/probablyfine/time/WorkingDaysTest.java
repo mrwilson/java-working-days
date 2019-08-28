af2cde8aa68b06b00a5ffa3ee8b3ac334c45d17a
Alex Wilson <alex+github@probablyfine.co.uk>
2019-08-28 18:33:49 +0100

    feat: First test.

--- src/test/java/uk/co/probablyfine/time/WorkingDaysTest.java
+++ src/test/java/uk/co/probablyfine/time/WorkingDaysTest.java
@@ -2,12 +2,24 @@ package uk.co.probablyfine.time;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.time.LocalDate;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class WorkingDaysTest {
 
     @Test
     public void shouldHandleWeekDays() {
-        assertTrue(true);
+        int workingDays = workingDaysBetween(
+            LocalDate.of(2019, 4, 1),
+            LocalDate.of(2019, 4, 5)
+        );
+
+        assertThat(workingDays, is(5));
+    }
+
+    private int workingDaysBetween(LocalDate start, LocalDate end) {
+        return 5;
     }
 }
