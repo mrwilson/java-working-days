cf954d86bf1c649845196848444e494f4d4b47ee
Alex Wilson <alex+github@probablyfine.co.uk>
2019-08-28 19:26:55 +0100

    refactor: Extract class

--- src/test/java/uk/co/probablyfine/time/WorkingDaysTest.java
+++ src/test/java/uk/co/probablyfine/time/WorkingDaysTest.java
@@ -11,44 +11,49 @@ import org.junit.Test;
 
 public class WorkingDaysTest {
 
+    private final WorkingDays wDays = new WorkingDays();
+
     @Test
     public void shouldHandleWeekDays() {
-        LocalDate workingDays = workingDaysAfter(date(2019, 4, 1), 4);
+        LocalDate workingDays = wDays.after(date(2019, 4, 1), 4);
 
         assertThat(workingDays, is(date(2019, 4, 5)));
     }
 
     @Test
     public void shouldHandleWeekDaysOverWeekend() {
-        LocalDate workingDays = workingDaysAfter(date(2019, 4, 1), 5);
+        LocalDate workingDays = wDays.after(date(2019, 4, 1), 5);
 
         assertThat(workingDays, is(date(2019, 4, 8)));
     }
 
     @Test
     public void shouldHandleWeekDaysOverEndOfFeb_NoLeapYear() {
-        LocalDate workingDays = workingDaysAfter(date(2019, 2, 28), 1);
+        LocalDate workingDays = wDays.after(date(2019, 2, 28), 1);
 
         assertThat(workingDays, is(date(2019, 3, 1)));
     }
 
     @Test
     public void shouldHandleWeekDaysOverEndOfFeb_LeapYear() {
-        LocalDate workingDays = workingDaysAfter(date(2012, 2, 28), 2);
+        LocalDate workingDays = wDays.after(date(2012, 2, 28), 2);
 
         assertThat(workingDays, is(date(2012, 3, 1)));
     }
 
-    private LocalDate workingDaysAfter(LocalDate start, int daysAfter) {
-        return start.datesUntil(date(2099, 1, 1))
-                .filter(this::isAWeekDay)
-                .limit(daysAfter + 1)
-                .collect(toList())
-                .get(daysAfter);
-    }
+    private static class WorkingDays {
 
-    private boolean isAWeekDay(LocalDate date) {
-        return !(date.getDayOfWeek() == SATURDAY || date.getDayOfWeek() == SUNDAY);
+        private LocalDate after(LocalDate start, int daysAfter) {
+            return start.datesUntil(LocalDate.of(2099, 1, 1))
+                    .filter(this::isAWeekDay)
+                    .limit(daysAfter + 1)
+                    .collect(toList())
+                    .get(daysAfter);
+        }
+
+        private boolean isAWeekDay(LocalDate date) {
+            return !(date.getDayOfWeek() == SATURDAY || date.getDayOfWeek() == SUNDAY);
+        }
     }
 
     private LocalDate date(int year, int month, int day) {
