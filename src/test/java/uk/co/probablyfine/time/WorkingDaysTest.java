39a5e3debe8e79a4a271faa8105458657955cbc8
Alex Wilson <alex+github@probablyfine.co.uk>
2019-08-28 18:28:40 +0100

    feat: Initial commit

---
+++ src/test/java/uk/co/probablyfine/time/WorkingDaysTest.java
@@ -0,0 +1,13 @@
+package uk.co.probablyfine.time;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class WorkingDaysTest {
+
+    @Test
+    public void shouldHandleWeekDays() {
+        assertTrue(true);
+    }
+}
