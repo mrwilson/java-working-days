486510c38aecb0ef7ee61ec67aa65c7a231b120d
Alex Wilson <alex+github@probablyfine.co.uk>
2019-08-28 18:51:51 +0100

    feat: Handle weekends
    * Requires upgrade to Java 11 to use `daysUntil` API

--- src/test/java/uk/co/probablyfine/time/WorkingDaysTest.java
+++ src/test/java/uk/co/probablyfine/time/WorkingDaysTest.java
@@ -2,7 +2,9 @@ package uk.co.probablyfine.time;
 
 import org.junit.Test;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -12,14 +14,31 @@ public class WorkingDaysTest {
     @Test
     public void shouldHandleWeekDays() {
         LocalDate workingDays = workingDaysAfter(
-            LocalDate.of(2019, 4, 1),
-            4
+                LocalDate.of(2019, 4, 1),
+                4
         );
 
         assertThat(workingDays, is(LocalDate.of(2019, 4, 5)));
     }
 
-    private LocalDate workingDaysAfter(LocalDate start, int daysAfter) {
-        return LocalDate.of(2019, 4, 5);
+    @Test
+    public void shouldHandleWeekDaysOverWeekend() {
+        LocalDate workingDays = workingDaysAfter(
+                LocalDate.of(2019, 4, 1),
+                5
+        );
+
+        assertThat(workingDays, is(LocalDate.of(2019, 4, 8)));
     }
+
+    private LocalDate workingDaysAfter(LocalDate start, int daysAfter) {
+
+        return start.datesUntil(LocalDate.of(2099, 1, 1))
+                .filter(date -> !(date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY))
+                .limit(daysAfter + 1)
+                .collect(Collectors.toList())
+                .get(daysAfter);
+    }
+
+
 }
