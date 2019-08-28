df698ffc4de93012d3e93b8c069ab74fb36a392b
Alex Wilson <alex+github@probablyfine.co.uk>
2019-08-28 19:37:11 +0100

    feat: Does not count Christmas Day or Boxing Day
    * This is proof-of-concept for public holidays in general.

--- src/test/java/uk/co/probablyfine/time/WorkingDaysTest.java
+++ src/test/java/uk/co/probablyfine/time/WorkingDaysTest.java
@@ -2,11 +2,13 @@ package uk.co.probablyfine.time;
 
 import static java.time.DayOfWeek.SATURDAY;
 import static java.time.DayOfWeek.SUNDAY;
+import static java.util.function.Predicate.not;
 import static java.util.stream.Collectors.toList;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 import java.time.LocalDate;
+import java.time.Month;
 import org.junit.Test;
 
 public class WorkingDaysTest {
@@ -41,14 +43,28 @@ public class WorkingDaysTest {
         assertThat(twoDaysAfter, is(date(2012, 3, 1)));
     }
 
+    @Test
+    public void shouldHandleChristmasBankHolidays() {
+        LocalDate dayAfter = workingDays.after(date(2019, 12, 24), 1);
+
+        assertThat(dayAfter, is(date(2019, 12, 27)));
+    }
+
     private static class WorkingDays {
 
         private LocalDate after(LocalDate start, int daysAfter) {
             return start.datesUntil(LocalDate.of(2099, 1, 1))
                     .filter(this::isAWeekDay)
+                    .filter(not(this::isChristmasDayOrBoxingDay))
                     .limit(daysAfter + 1)
                     .collect(toList())
                     .get(daysAfter);
+        }
+
+        private boolean isChristmasDayOrBoxingDay(LocalDate date) {
+            int day = date.getDayOfMonth();
+
+            return date.getMonth() == Month.DECEMBER && (day == 25 || day == 26);
         }
 
         private boolean isAWeekDay(LocalDate date) {
