91a2ce560a26b2269a9d54fa8466a64ea3ea6227
Alex Wilson <alex+github@probablyfine.co.uk>
2019-08-31 15:48:20 +0100

    feat(calendar-us): Handle Independence Day

--- src/main/java/uk/co/probablyfine/time/calendars/UnitedStates.java
+++ src/main/java/uk/co/probablyfine/time/calendars/UnitedStates.java
@@ -1,5 +1,7 @@
 package uk.co.probablyfine.time.calendars;
 
+import static uk.co.probablyfine.time.calendars.HolidayCalendar.isTheWeekend;
+
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Month;
@@ -7,7 +9,10 @@ import java.time.Month;
 public class UnitedStates implements HolidayCalendar {
     @Override
     public boolean isPublicHoliday(LocalDate date) {
-        return isMlkJrDay(date) || isPresidentsDay(date) || isMemorialDay(date);
+        return isMlkJrDay(date)
+                || isPresidentsDay(date)
+                || isMemorialDay(date)
+                || isIndependenceDay(date);
     }
 
     private boolean isMlkJrDay(LocalDate date) {
@@ -28,5 +33,17 @@ public class UnitedStates implements HolidayCalendar {
         return date.getMonth() == Month.MAY
                 && date.getDayOfWeek() == DayOfWeek.MONDAY
                 && date.getDayOfMonth() >= 25;
+    }
+
+    private boolean isIndependenceDay(LocalDate date) {
+        if (date.getMonth() != Month.JULY) {
+            return false;
+        }
+
+        boolean asNormal = date.getDayOfMonth() == 4 && !isTheWeekend(date);
+        boolean onSaturday = date.getDayOfMonth() == 3 && date.getDayOfWeek() == DayOfWeek.FRIDAY;
+        boolean onSunday = date.getDayOfMonth() == 5 && date.getDayOfWeek() == DayOfWeek.MONDAY;
+
+        return asNormal || onSaturday || onSunday;
     }
 }
