b30df1518564ff3bb9be5f6e2abf3cf0f00b9035
Alex Wilson <alex+github@probablyfine.co.uk>
2019-08-31 15:39:19 +0100

    feat(calendar-us): Handle Memorial Day

--- src/test/java/uk/co/probablyfine/time/WorkingDaysUnitedStatesTest.java
+++ src/test/java/uk/co/probablyfine/time/WorkingDaysUnitedStatesTest.java
@@ -27,4 +27,13 @@ public class WorkingDaysUnitedStatesTest implements WorkingDaysTest {
         // President's Day is 17th February in 2020
         assertWorkingDaysRelationship(date(2020, 2, 14), 1, date(2020, 2, 18));
     }
+
+    @Test
+    public void shouldHandleMemorialDay() {
+        // Memorial Day was 27th May in 2019
+        assertWorkingDaysRelationship(date(2019, 5, 24), 1, date(2019, 5, 28));
+
+        // Memorial Day is 25th May in 2020
+        assertWorkingDaysRelationship(date(2020, 5, 22), 1, date(2020, 5, 26));
+    }
 }
