310e3b5b017fd66bb63e5ac6afaae5988dc1f884
Alex Wilson <alex+github@probablyfine.co.uk>
2019-09-01 16:54:34 +0100

    refactor(tests): Consolidate common logic for historic tests

--- src/test/java/uk/co/probablyfine/time/WorkingDaysEnglandWalesHistoricTest.java
+++ src/test/java/uk/co/probablyfine/time/WorkingDaysEnglandWalesHistoricTest.java
@@ -1,34 +1,27 @@
 package uk.co.probablyfine.time;
 
 import static org.junit.Assert.assertTrue;
+import static uk.co.probablyfine.time.WorkingDaysHistoricTest.loadExamples;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.util.stream.Collectors;
+import java.util.Collection;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+import uk.co.probablyfine.time.WorkingDaysHistoricTest.Example;
 import uk.co.probablyfine.time.calendars.HolidayCalendar;
 
 @RunWith(Parameterized.class)
 public class WorkingDaysEnglandWalesHistoricTest {
 
-    private Example example;
-
     @Parameters(name = "{0}")
-    public static Iterable<Object> data() throws IOException {
-        return Files.readAllLines(ENGLAND_AND_WALES).stream()
-                .map(WorkingDaysEnglandWalesHistoricTest::toExample)
-                .collect(Collectors.toList());
+    public static Collection<Example> data() throws IOException {
+        return loadExamples("src/test/resources/historic_dates_england_and_wales.csv");
     }
 
-    private static final Path ENGLAND_AND_WALES =
-            Paths.get("src/test/resources/historic_dates_england_and_wales.csv");
-    private static final HolidayCalendar calendar = HolidayCalendar.ENGLAND_AND_WALES;
+    private final HolidayCalendar calendar = HolidayCalendar.ENGLAND_AND_WALES;
+    private final Example example;
 
     public WorkingDaysEnglandWalesHistoricTest(Example example) {
         this.example = example;
@@ -39,25 +32,5 @@ public class WorkingDaysEnglandWalesHistoricTest {
         String message = String.format("%s was not detected", example);
 
         assertTrue(message, calendar.isPublicHoliday(example.date));
-    }
-
-    private static Example toExample(String input) {
-        String[] split = input.split(",");
-        return new Example(split[0], LocalDate.parse(split[1]));
-    }
-
-    static class Example {
-        final String name;
-        final LocalDate date;
-
-        Example(String name, LocalDate date) {
-            this.name = name;
-            this.date = date;
-        }
-
-        @Override
-        public String toString() {
-            return String.format("%s %s", name, date);
-        }
     }
 }
