0875032a84a5cad2df8c71bceec625c45d9f5f3e
Alex Wilson <alex+github@probablyfine.co.uk>
2019-09-01 16:54:34 +0100

    refactor(tests): Consolidate common logic for historic tests

---
+++ src/test/java/uk/co/probablyfine/time/WorkingDaysHistoricTest.java
@@ -0,0 +1,37 @@
+package uk.co.probablyfine.time;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public interface WorkingDaysHistoricTest {
+
+    static Example toExample(String input) {
+        String[] split = input.split(",");
+        return new Example(split[0], LocalDate.parse(split[1]));
+    }
+
+    static Collection<Example> loadExamples(String path) throws IOException {
+        return Files.readAllLines(Paths.get(path)).stream()
+                .map(WorkingDaysHistoricTest::toExample)
+                .collect(Collectors.toList());
+    }
+
+    class Example {
+        final String name;
+        final LocalDate date;
+
+        Example(String name, LocalDate date) {
+            this.name = name;
+            this.date = date;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s %s", name, date);
+        }
+    }
+}
--- src/test/java/uk/co/probablyfine/time/WorkingDaysUnitedStatesHistoricTest.java
+++ src/test/java/uk/co/probablyfine/time/WorkingDaysUnitedStatesHistoricTest.java
@@ -1,34 +1,27 @@
 package uk.co.probablyfine.time;
 
 import static org.junit.Assert.assertTrue;
+import static uk.co.probablyfine.time.WorkingDaysHistoricTest.loadExamples;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.util.stream.Collectors;
+import java.util.Collection;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+import uk.co.probablyfine.time.WorkingDaysHistoricTest.Example;
 import uk.co.probablyfine.time.calendars.HolidayCalendar;
 
 @RunWith(Parameterized.class)
 public class WorkingDaysUnitedStatesHistoricTest {
 
-    private Example example;
-
     @Parameters(name = "{0}")
-    public static Iterable<Object> data() throws IOException {
-        return Files.readAllLines(UNITED_STATES).stream()
-                .map(WorkingDaysUnitedStatesHistoricTest::toExample)
-                .collect(Collectors.toList());
+    public static Collection<Example> data() throws IOException {
+        return loadExamples("src/test/resources/historic_dates_united_states.csv");
     }
 
-    private static final Path UNITED_STATES =
-            Paths.get("src/test/resources/historic_dates_united_states.csv");
-    private static final HolidayCalendar calendar = HolidayCalendar.UNITED_STATES;
+    private final HolidayCalendar calendar = HolidayCalendar.UNITED_STATES;
+    private final Example example;
 
     public WorkingDaysUnitedStatesHistoricTest(Example example) {
         this.example = example;
@@ -47,26 +40,6 @@ public class WorkingDaysUnitedStatesHistoricTest {
                 break;
             default:
                 assertTrue(message, calendar.isPublicHoliday(example.date));
-        }
-    }
-
-    private static Example toExample(String input) {
-        String[] split = input.split(",");
-        return new Example(split[0], LocalDate.parse(split[1]));
-    }
-
-    static class Example {
-        final String name;
-        final LocalDate date;
-
-        Example(String name, LocalDate date) {
-            this.name = name;
-            this.date = date;
-        }
-
-        @Override
-        public String toString() {
-            return String.format("%s %s", name, date);
         }
     }
 }
