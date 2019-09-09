83a379ef5ee3cecfd65ce11c9b87314b70780dd7
Alex Wilson <alex+github@probablyfine.co.uk>
2019-09-09 18:28:08 +0100

    feat(calendar-fr): Handle Lundi de Pâques

--- src/main/java/uk/co/probablyfine/time/calendars/France.java
+++ src/main/java/uk/co/probablyfine/time/calendars/France.java
@@ -1,6 +1,7 @@
 package uk.co.probablyfine.time.calendars;
 
 import static java.time.DayOfWeek.MONDAY;
+import static uk.co.probablyfine.time.calendars.Easter.easterSunday;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -8,7 +9,7 @@ import java.time.Month;
 public class France implements HolidayCalendar {
     @Override
     public boolean isPublicHoliday(LocalDate date) {
-        return isNewYearsDay(date);
+        return isNewYearsDay(date) || isLundiDePaques(date);
     }
 
     private boolean isNewYearsDay(LocalDate date) {
@@ -20,5 +21,9 @@ public class France implements HolidayCalendar {
         boolean onSunday = date.getDayOfWeek() == MONDAY && date.getDayOfMonth() == 2;
 
         return duringTheWeek || onSunday;
+    }
+
+    private boolean isLundiDePaques(LocalDate date) {
+        return date.getDayOfMonth() == easterSunday(date.getYear()).plusDays(1).getDayOfMonth();
     }
 }
--- src/test/java/uk/co/probablyfine/time/calendars/FranceTest.java
+++ src/test/java/uk/co/probablyfine/time/calendars/FranceTest.java
@@ -26,4 +26,16 @@ public class FranceTest implements WorkingDaysTest {
         // Jour de l'an is observed on 2nd January in 2023
         assertWorkingDaysRelationship(date(2022, 12, 30), 1, date(2023, 1, 3));
     }
+
+    @Test
+    public void shouldHandleLundiDePaques() {
+        // Lundi de Pâques was observed on 1st January in 2019
+        assertWorkingDaysRelationship(date(2019, 4, 19), 1, date(2019, 4, 23));
+
+        // Lundi de Pâques has no Public Holiday in 2022
+        assertWorkingDaysRelationship(date(2020, 4, 10), 1, date(2020, 4, 14));
+
+        // Lundi de Pâques is observed on 2nd January in 2023
+        assertWorkingDaysRelationship(date(2021, 4, 2), 1, date(2021, 4, 6));
+    }
 }
